0e102fb92408183da5519a7f7416927aeb82458a
ordonezmarco <ordonez.marcos21@gmail.com>
2019-08-19 23:35:48 -0500

    2.1.1

---
+++ Lands2/Lands2/ViewModels/LoginViewModel.cs
@@ -0,0 +1,64 @@
+﻿
+
+namespace Lands2.ViewModels
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+    using System.Windows.Input;
+    using GalaSoft.MvvmLight.Command;
+
+    class LoginViewModel
+    {
+        #region Properties
+        public string Email 
+        {
+            get;
+            set;
+        }
+        public string Password
+        {
+            get;
+            set;
+        }
+        public bool IsRunning
+        {
+            get;
+            set;
+        }
+        public bool IsRemembered
+        {
+            get;
+            set;
+        }
+        #endregion
+
+        #region Commands
+        public ICommand LoginCommand
+        {
+            get
+            {
+                return new RelayCommand(Login);
+            }
+        }
+
+        private void Login()
+        {
+
+        }
+        public ICommand RegisterCommand
+        {
+            get;
+            set;
+        }
+        #endregion
+
+        #region Constructors
+        public LoginViewModel()
+        {
+            this.IsRemembered = true;
+        }
+        #endregion
+
+    }
+}
--- Lands2/Lands2/ViewModels/MainViewModel.cs
+++ Lands2/Lands2/ViewModels/MainViewModel.cs
@@ -6,6 +6,19 @@ namespace Lands2.ViewModels
 {
     class MainViewModel
     {
+        #region ViewModels
+        public LoginViewModel Login
+        {
+            get;
+            set;
+        }
+        #endregion
 
+        #region Constructors
+        public MainViewModel()
+        {
+            this.Login = new LoginViewModel();
+        }
+        #endregion
     }
 }

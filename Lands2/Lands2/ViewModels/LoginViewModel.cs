32d16c622fde7988cc370f2fc4df9d26b06931bb
ordonezmarco <ordonez.marcos21@gmail.com>
2019-08-20 22:11:19 -0500

    Antes de Ajustar BaseViewModel

--- Lands2/Lands2/ViewModels/LoginViewModel.cs
+++ Lands2/Lands2/ViewModels/LoginViewModel.cs
@@ -4,32 +4,89 @@ namespace Lands2.ViewModels
 {
     using System;
     using System.Collections.Generic;
+    using System.ComponentModel;
     using System.Text;
     using System.Windows.Input;
     using GalaSoft.MvvmLight.Command;
+    using Xamarin.Forms;
 
-    class LoginViewModel
+    class LoginViewModel : INotifyPropertyChanged
     {
+        #region Events
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        #endregion
+
+        #region Attirbutes
+        private string email;
+        private string password;
+        private bool isRunning;
+        private bool isEnable;
+        #endregion
+
         #region Properties
-        public string Email 
+        public string Email
         {
             get;
             set;
         }
         public string Password
         {
-            get;
-            set;
+            get
+            {
+                return this.password;
+            }
+            set
+            {
+                if(this.password != value)
+                {
+                    this.password = value;
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(this.Password)));
+                }
+            }
         }
         public bool IsRunning
         {
-            get;
-            set;
+            get
+            {
+                return this.isRunning;
+            }
+            set
+            {
+                if (this.isRunning != value)
+                {
+                    this.isRunning = value;
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(this.IsRunning)));
+                }
+            }
         }
         public bool IsRemembered
         {
             get;
             set;
+        }
+        public bool IsEnable
+        {
+            get
+            {
+                return this.isEnable;
+            }
+            set
+            {
+                if (this.isEnable != value)
+                {
+                    this.isEnable = value;
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(this.IsEnable)));
+                }
+            }
+        }
+        #endregion
+
+        #region Constructors
+        public LoginViewModel()
+        {
+            this.IsRemembered = true;
+            this.isEnable = true;
         }
         #endregion
 
@@ -42,8 +99,50 @@ namespace Lands2.ViewModels
             }
         }
 
-        private void Login()
+        private async void Login()
         {
+            if (string.IsNullOrEmpty( this.Email))
+            {
+                await Application.Current.MainPage.DisplayAlert(
+                    "Error",
+                    "Your must enter an email.",
+                    "Accept");
+                return;
+            }
+
+            if (string.IsNullOrEmpty(this.Password))
+            {
+                await Application.Current.MainPage.DisplayAlert(
+                    "Error",
+                    "Your must enter a password.",
+                    "Accept");
+                return;
+            }
+
+            this.IsRunning = true;
+            this.IsEnable = false;
+
+            if (this.Email != "ordonez.marcos21@gmail.com" || this.Password != "123456")
+            {
+                this.IsRunning = false;
+                this.IsEnable = true;
+
+                await Application.Current.MainPage.DisplayAlert(
+                    "Error",
+                    "Email or password incorrect.",
+                    "Accept");
+                this.Password = string.Empty;
+
+                return;
+            }
+
+            this.IsRunning = false;
+            this.IsEnable = true;
+
+            await Application.Current.MainPage.DisplayAlert(
+                    "Ok",
+                    "Login Succesful.",
+                    "Accept");
 
         }
         public ICommand RegisterCommand
@@ -52,13 +151,5 @@ namespace Lands2.ViewModels
             set;
         }
         #endregion
-
-        #region Constructors
-        public LoginViewModel()
-        {
-            this.IsRemembered = true;
-        }
-        #endregion
-
     }
 }

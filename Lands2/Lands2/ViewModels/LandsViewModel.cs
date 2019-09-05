c04a51b0212b3ae6c80a1bfef1975c9ef62b47b5
ordonezmarco <ordonez.marcos21@gmail.com>
2019-09-04 20:07:56 -0500

    ListViewAnexos

--- Lands2/Lands2/ViewModels/LandsViewModel.cs
+++ Lands2/Lands2/ViewModels/LandsViewModel.cs
@@ -5,6 +5,9 @@ namespace Lands2.ViewModels
     using System;
     using System.Collections.Generic;
     using System.Collections.ObjectModel;
+    using System.Linq;
+    using System.Windows.Input;
+    using GalaSoft.MvvmLight.Command;
     using Models;
     using Services;
     using Xamarin.Forms;
@@ -21,6 +24,9 @@ namespace Lands2.ViewModels
 
         #region Attributes
         private ObservableCollection<Land> lands;
+        private bool isRefreshing;
+        private string filter;
+        private List<Land> landList;
         #endregion
 
         #region Properties
@@ -28,6 +34,20 @@ namespace Lands2.ViewModels
         {
             get { return this.lands; }
             set { SetValue(ref this.lands, value); }
+        }
+        public bool IsRefreshing
+        {
+            get { return this.isRefreshing; }
+            set { SetValue(ref this.isRefreshing, value); }
+        }
+        public string Filter
+        {
+            get { return this.filter; }
+            set
+            {
+                SetValue(ref this.filter, value);
+                this.Search();
+            }
         }
         #endregion
 
@@ -42,10 +62,12 @@ namespace Lands2.ViewModels
         #region Methods
         private async void LoadLands()
         {
+            this.IsRefreshing = true;
             var connection = await this.apiService.CheckConnection();
 
             if(!connection.IsSuccess)
             {
+                this.IsRefreshing = false;
                 await Application.Current.MainPage.DisplayAlert("Error", connection.Message, "Accept");
                 await Application.Current.MainPage.Navigation.PopAsync();
                 return;
@@ -54,17 +76,50 @@ namespace Lands2.ViewModels
             var response = await this.apiService.GetList<Land>("http://restcountries.eu", "/rest", "/v2/all");
             if(!response.IsSuccess)
             {
+                this.IsRefreshing = false;
                 await Application.Current.MainPage.DisplayAlert("Error",response.Message,"Accept");
                 await Application.Current.MainPage.Navigation.PopAsync();
                 return;
             }
 
-            var list = (List<Land>)response.Result;
-            this.Lands = new ObservableCollection<Land>(list);
-
-        } 
+            //var list =  (List<Land>)response.Result;
+            this.landList = (List<Land>)response.Result;
+            //this.Lands = new ObservableCollection<Land>(list);
+            this.Lands = new ObservableCollection<Land>(this.landList);
+            this.IsRefreshing = false;
+        }
+        private void Search()
+        {
+            if(string.IsNullOrEmpty(this.Filter))
+            {
+                this.Lands = new ObservableCollection<Land>(this.landList);
+            }
+            else
+            {
+                this.Lands = new ObservableCollection<Land>(
+                    this.landList.Where(
+                        l => l.Name.ToLower().Contains(this.Filter.ToLower()) ||
+                             l.Capital.ToLower().Contains(this.Filter.ToLower())
+                        ));
+            }
+        }
         #endregion
 
-
+        #region Commands
+        public ICommand RefreshCommand
+        {
+            get
+            {
+                return new RelayCommand(LoadLands);
+            }     
+        }
+        public ICommand SearchCommand
+        {
+            get
+            {
+                return new RelayCommand(Search);
+            }
+        }
+        #endregion
     }
 }
